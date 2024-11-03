8ee51e09e9a62363b791ba159830434925ba2777
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-11-03 18:13:33 +0000

    Initial Commit

---
+++ GROUP_PROJECT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GROUP_PROJECT
+//
+//  Created by Joseph Mccole on 03/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GROUP_PROJECTTests/GROUP_PROJECTTests.swift
@@ -0,0 +1,17 @@
+//
+//  GROUP_PROJECTTests.swift
+//  GROUP_PROJECTTests
+//
+//  Created by Joseph Mccole on 03/11/2024.
+//
+
+import Testing
+@testable import GROUP_PROJECT
+
+struct GROUP_PROJECTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
